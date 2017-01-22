3ff1982694513b3dfa7c51a60681aa408cf21bb6
pguerrerox <pguerrerox@gmail.com>
2017-01-22 14:26:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,132 @@
+// REQUIREMENTS
+var express = require("express");
+var multer = require("multer");
+var logger = require("morgan");
+var path = require('path');
+var settings = require('./settings.js');
+// var bodyParser = require("body-parser");
+// var fs = require('fs');
+
+// MAKE EXPRESS APP
+var app = express();
+var server = require('http').createServer(app);
+
+// LOGGER, only log error responses
+app.use(logger("dev"));
+
+// GETS
+app.get("/status", function(req,res){
+	res.send("StaticMail is running...");
+});
+
+// MULTER multipart/form-data handler
+var uploadFile = multer({
+	// fileFilter config
+	fileFilter: function (req, file, cb) {
+		var filetypes = /pdf/;
+		var mimetype = filetypes.test(file.mimetype);
+		var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+		if (mimetype && extname) {
+			// console.log(mimetype, extname);
+			return cb(null, true);
+		}
+		cb(null, false);
+		// console.log('error, no es pdf');
+	},
+	// sizeLimit config
+	limits: {
+		fieldNameSize: 100,
+		fileSize: 2*1000*1000, // 2MB limit
+		files: 1,
+	},
+});
+
+// POST JEMAZAR.COM/CONTACTO
+app.post("/jemazar-contacto", uploadFile.single(), function(req,res){
+	var api_key = settings.api_key;
+  var domain = settings.domain;
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+	var data = {
+		from: settings.from, //replace with your SMTP Login ID
+		to: settings.toTest, //recipients email
+		subject: req.body.tema,
+		html:    '<html>'+
+    '<head>    <style>      .content{  width:600px;  margin:0 auto;  padding:15px;  background-color:#003399;  box-shadow: 0 0 15px #000 ;}    h1, h3{  text-align:center;  color:#ffffff;}    p{  text-align:center;  margin:30px 60px;  color: #ffffff;}    a{  background-color:#ffffff;  color:#003399;  font-weight:bold;  padding: 3px}    a:hover{   background-color:#000;}    .info{  width:70%;  margin:20px auto 20px auto;  padding:25px;  background-color:#eee;  }    span{  display:inline-block;  margin:0 auto;  font-weight: bold;  padding:5px 10px 5px 100px;  }  </style></head>'+
+    '<body>      <div class="content">        <h1>Has recibido un mensaje!</h1>        <p>Este mensaje proviene desde <a href="http://jemazar.com/pages/contacto.html">jemazar.com/contacto</a>, contiene datos introducidos por un usuario en el <b>formulario de contacto</b></p>        <br />        <h3><b>Informacion del Formulario</b></h3>        <div class="info">'+
+    '<span>Nombre: </span>'+req.body.name+'<br />'+
+    '<span>Email: </span>'+req.body.email+'<br />'+
+    '<span>Ciudad: </span>'+req.body.ciudad+'<br />'+
+    '<span>Asunto: </span>'+req.body.asunto+'<br />'+
+    '<span>Mensaje: </span>'+req.body.mensaje+'<br />'+
+    '</div></div></body></html>'
+  };
+	mailgun.messages().send(data, function (error, body) {
+		// console.log(data);
+    if(!error){
+			res.redirect('http://jemazar.com/pages/form-status-ok.html');
+		}
+		// else
+		// 	res.redirect('http://jemazar.com/pages/form-status-error.html');
+  });
+});
+
+// POST JEMAZAR.COM/EMPLEO
+app.post("/jemazar-empleo", uploadFile.single('afile'), function(req,res){
+  var api_key = settings.api_key;
+  var domain = settings.domain;
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+  var upFile = req.file.buffer;
+  var upFileName = req.file.originalname;
+  var upFileSize = req.file.size;
+  var upFileType = req.file.mimetype;
+  var attch = new mailgun.Attachment({
+    data: upFile,
+    filename: upFileName,
+    knownLength: upFileSize,
+    contentType: upFileType
+  });
+  var data = {
+		from: settings.from, //replace with your SMTP Login ID
+		to: settings.toTest, //recipients email
+    subject: 'Curriculum Vitae (CV) de '+req.body.name,
+		attachment: attch,
+		html:    '<html>'+
+    '<head>    <style>      .content{  width:600px;  margin:0 auto;  padding:15px;  background-color:#003399;  box-shadow: 0 0 15px #000 ;}    h1, h3{  text-align:center;  color:#ffffff;}    p{  text-align:center;  margin:30px 60px;  color: #ffffff;}    a{  background-color:#ffffff;  color:#003399;  font-weight:bold;  padding: 3px}    a:hover{   background-color:#000;}    .info{  width:70%;  margin:20px auto 20px auto;  padding:25px;  background-color:#eee;  }    span{  display:inline-block;  margin:0 auto;  font-weight: bold;  padding:5px 10px 5px 100px;  }  </style></head>'+
+    '<body>      <div class="content">        <h1>Has recibido un mensaje!</h1>        <p>Este mensaje proviene desde <a href="http://jemazar.com/pages/empleo.html">jemazar.com/contacto</a>, contiene datos introducidos por un usuario en el <b>formulario de contacto</b></p>        <br />        <h3><b>Informacion del Formulario</b></h3>        <div class="info">'+
+    '<span>Nombre: </span>'+req.body.name+'<br />'+
+    '<span>Email: </span>'+req.body.email+'<br />'+
+    '</div></div></body></html>'
+  };
+	mailgun.messages().send(data, function (error, body) {
+		// console.log(data);
+		if(!error){
+			res.redirect('http://jemazar.com/pages/form-status-ok.html');
+		}
+		// else
+		// 	res.redirect('http://jemazar.com/pages/form-status-error.html');
+	});
+});
+
+// ERROR DISPLAY
+app.use(function (err, req, res, next) {
+	if (err.code === 'LIMIT_FILE_SIZE') {
+		console.log('File is too big');
+		// console.log(err.code);
+		// res.send({ error: 'File is too big' });
+		res.redirect('http://jemazar.com/pages/form-status-error.html');
+		return;
+	}
+	if(err){
+		console.log('Solo PDF');
+		// console.log(err);
+		// res.send({ error: 'Solo PDF' });
+		res.redirect('http://jemazar.com/pages/form-status-error.html');
+	}
+	// Handle any other errors
+});
+
+// Listen for an application request on port 8081
+server.listen(8081, function () {
+  console.log('Nodejs app listening on http://127.0.0.1:8081/');
+});
